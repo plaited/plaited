14ce6d7aafee1ba182ca148d09dfa628b97361c0
Edward Irby <e.irby@pm.me>
2023-01-17 17:09:55 -0600

    refactor: this is really a hook not a component

--- packages/island/src/use-hook.type.ts
+++ packages/island/src/use-hook.type.ts
@@ -1,7 +1,7 @@
 import { RulesFunc } from '@plaited/plait'
 import { Query } from './define-element.js'
 
-export type UseComponent<T = Record<string, unknown>> = (args: {
+export type UseHook<T = Record<string, unknown>> = (args: {
   $: Query
 } & T) => {
   actions: Record<string, (payload: unknown) => void>,
