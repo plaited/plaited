526977c471dd00588dc905dc4fc1ee8500ef8da1
Edward Irby <e.irby@pm.me>
2023-01-31 05:35:49 -0600

    refactor: testing format

--- src/server/get-reload-route.ts
+++ src/server/get-reload-route.ts
@@ -1,40 +1,42 @@
-import { Routes, ReloadClient, HandlerContext } from './types.ts'
+import { HandlerContext, ReloadClient, Routes } from './types.ts'
 
-const getMessage = (channel:string, data:string) => {
+const getMessage = (channel: string, data: string) => {
   const encoder = new TextEncoder()
   return encoder.encode(`event: ${channel}\nid: 0\ndata: ${data}\n\n\n`)
 }
 
 export const getReloadRoute = (
   reload: boolean,
-  reloadClient: Array<ReloadClient>
+  reloadClient: Array<ReloadClient>,
 ): Record<never, never> | Routes => {
-  if(!reload) return {}
+  if (!reload) return {}
   return {
     ['/livereload']: (req: Response, ctx: HandlerContext) => {
       let timerId: number | undefined
-      const init = ({
+      const init = {
         status: 200,
         headers: {
           connection: 'keep-alive',
           'content-type': 'text/event-stream',
           'cache-control': 'no-cache',
-        } })
+        },
+      }
       const body = new ReadableStream({
         start(controller) {
           reloadClient.push(
-            (channel: string, data: string) => controller.enqueue(getMessage(channel, data))
+            (channel: string, data: string) =>
+              controller.enqueue(getMessage(channel, data)),
           )
           controller.enqueue(getMessage('connected', 'ready'))
           timerId = setInterval(() => {
             controller.enqueue(getMessage('ping', 'waiting'))
           }, 60000)
-        }, 
-        cancel(){
+        },
+        cancel() {
           if (typeof timerId === 'number') {
             clearInterval(timerId)
           }
-        }, 
+        },
       })
       return new Response(body, init)
     },
