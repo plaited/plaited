f716bbfc5ea9ef0eacb2376672c5051da15e9c28
Edward Irby <e.irby@pm.me>
2023-02-06 15:51:46 -0600

    test(plait): refactor test to ensure array is zeroed out for each test

--- libs/plait/tests/plait.test.ts
+++ libs/plait/tests/plait.test.ts
@@ -1,4 +1,10 @@
-import { assertEquals, assertSnapshot } from '../../test-deps.ts'
+import {
+  assertEquals,
+  assertSnapshot,
+  beforeEach,
+  describe,
+  it,
+} from '../../test-deps.ts'
 import {
   block,
   chaosStrategy,
@@ -10,111 +16,118 @@ import {
   waitFor,
 } from '../mod.ts'
 
-const actualFeedback: string[] = []
-const expectedFeedback = [
-  'Add hot',
-  'Add cold',
-  'Add hot',
-  'Add cold',
-  'Add hot',
-  'Add cold',
-]
+describe('Plait', () => {
+  const actualFeedback: string[] = []
+  const expectedFeedback = [
+    'Add hot',
+    'Add cold',
+    'Add hot',
+    'Add cold',
+    'Add hot',
+    'Add cold',
+  ]
+  beforeEach(() => {
+    actualFeedback.length = 0
+  })
 
-const addHot = () => actualFeedback.push('Add hot')
-const addCold = () => actualFeedback.push('Add cold')
-const strands = {
-  addHot: strand(
-    waitFor<string>({
-      type: 'start',
-      assert({ data }) {
-        return data === 'start'
-      },
-    }),
-    request({ type: 'hot' }),
-    request({ type: 'hot' }),
-    request({ type: 'hot' }),
-  ),
-  addCold: strand(
-    waitFor({ type: 'start' }),
-    request({ type: 'cold' }),
-    request({ type: 'cold' }),
-    request({ type: 'cold' }),
-  ),
-  mixHotCold: loop(
-    strand(
-      Object.assign(
-        waitFor({ type: 'hot' }),
-        block({ type: 'cold' }),
-      ),
-      Object.assign(
-        waitFor({ type: 'cold' }),
-        block({ type: 'hot' }),
+  const addHot = () => actualFeedback.push('Add hot')
+  const addCold = () => actualFeedback.push('Add cold')
+  const strands = {
+    addHot: strand(
+      waitFor<string>({
+        type: 'start',
+        assert({ data }) {
+          return data === 'start'
+        },
+      }),
+      request({ type: 'hot' }),
+      request({ type: 'hot' }),
+      request({ type: 'hot' }),
+    ),
+    addCold: strand(
+      waitFor({ type: 'start' }),
+      request({ type: 'cold' }),
+      request({ type: 'cold' }),
+      request({ type: 'cold' }),
+    ),
+    mixHotCold: loop(
+      strand(
+        Object.assign(
+          waitFor({ type: 'hot' }),
+          block({ type: 'cold' }),
+        ),
+        Object.assign(
+          waitFor({ type: 'cold' }),
+          block({ type: 'hot' }),
+        ),
       ),
     ),
-  ),
-}
-const actions = {
-  cold() {
-    addCold()
-  },
-  hot() {
-    addHot()
-  },
-}
-Deno.test('plait(): priority queue', (t) => {
-  const streamLog: unknown[] = []
-  const { trigger, feedback, stream, add } = plait({ dev: true })
-  add(strands)
-  feedback(actions)
-  stream.subscribe((msg) => {
-    streamLog.push(msg)
+  }
+  const actions = {
+    cold() {
+      addCold()
+    },
+    hot() {
+      addHot()
+    },
+  }
+  it('priority queue', (t) => {
+    const streamLog: unknown[] = []
+    const { trigger, feedback, stream, add } = plait({ dev: true })
+    add(strands)
+    feedback(actions)
+    stream.subscribe((msg) => {
+      streamLog.push(msg)
+    })
+    trigger({
+      type: 'start',
+      data: 'start',
+    })
+    assertEquals(
+      actualFeedback,
+      expectedFeedback,
+      `priority selection feedback`,
+    )
+    assertSnapshot(t, streamLog, `priority selection feedback`)
   })
-  trigger({
-    type: 'start',
-    data: 'start',
+  it('randomized priority queue', (t) => {
+    const streamLog: unknown[] = []
+    const { trigger, feedback, stream, add } = plait({
+      strategy: randomizedStrategy,
+      dev: true,
+    })
+    add(strands)
+    feedback(actions)
+    stream.subscribe((msg) => {
+      streamLog.push(msg)
+    })
+    trigger({
+      type: 'start',
+      data: 'start',
+    })
+    assertEquals(
+      actualFeedback,
+      expectedFeedback,
+      `randomized priority selection feedback`,
+    )
+    assertSnapshot(t, streamLog, `randomized priority selection log`)
   })
-  assertEquals(actualFeedback, expectedFeedback, `priority selection feedback`)
-  assertSnapshot(t, streamLog, `priority selection feedback`)
-})
-Deno.test('plait(): randomized priority queue', (t) => {
-  const streamLog: unknown[] = []
-  actualFeedback.length = 0
-  const { trigger, feedback, stream, add } = plait({
-    strategy: randomizedStrategy,
-    dev: true,
+  it('chaos selection', (t) => {
+    const streamLog: unknown[] = []
+    const { trigger, feedback, stream, add } = plait({
+      strategy: chaosStrategy,
+      dev: true,
+    })
+    add(strands)
+    feedback(actions)
+    stream.subscribe((msg) => {
+      streamLog.push(msg)
+    })
+    trigger({
+      type: 'start',
+      data: 'start',
+    })
+    assertEquals(actualFeedback, expectedFeedback, `chaos selection feedback`)
+    assertSnapshot(t, streamLog, `chaos selection log`)
   })
-  add(strands)
-  feedback(actions)
-  stream.subscribe((msg) => {
-    streamLog.push(msg)
-  })
-  trigger({
-    type: 'start',
-    data: 'start',
-  })
-  assertEquals(
-    actualFeedback,
-    expectedFeedback,
-    `randomized priority selection feedback`,
-  )
-  assertSnapshot(t, streamLog, `randomized priority selection log`)
-})
-Deno.test('plait(): chaos selection', (t) => {
-  const streamLog: unknown[] = []
-  actualFeedback.length = 0
-  const { trigger, feedback, stream, add } = plait({
-    strategy: chaosStrategy,
-    dev: true,
-  })
-  add(strands)
-  feedback(actions)
-  stream.subscribe((msg) => {
-    streamLog.push(msg)
-  })
-  trigger({
-    type: 'start',
-    data: 'start',
-  })
-  assertEquals(actualFeedback, expectedFeedback, `chaos selection feedback`)
-  assertSnapshot(t, streamLog, `chaos selection log`)
 })
