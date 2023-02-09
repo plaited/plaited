ede3cff71c79fbb62260514b1350ae472bcef36c
Edward Irby <e.irby@pm.me>
2023-02-09 04:48:47 -0600

    refactor(servor): prework before server rewrite

--- libs/server/create-server.ts
+++ libs/server/create-server.ts
@@ -1,7 +1,6 @@
-import { extname, serve, serveTls } from '../deps.ts'
-import { CreateServer } from './types.ts'
-import { getFileHandler } from './get-file-handler.ts'
-
+import { compress, extname, serve, serveDir, serveTls } from '../deps.ts'
+import { CreateServer, HandlerContext } from './types.ts'
+import { mimeTypes } from './mime-types.ts'
 export const createServer: CreateServer = ({
   handler,
   credentials,
@@ -12,19 +11,41 @@ export const createServer: CreateServer = ({
 }) => {
   const createServer = credentials ? serveTls : serve
   createServer(async (
-    req,
-    ctx,
+    req: Request,
+    ctx: HandlerContext,
   ) => {
     const { pathname } = new URL(req.url)
     const fileExt = extname(pathname)
-    return fileExt
-      ? await getFileHandler({
-        pathname,
-        fileExt,
-        root,
-        req,
+    if (fileExt) {
+      const ext: string = fileExt.slice(1)
+      if (
+        ['js', 'mjs', 'css', 'html', 'htm', 'json', 'xml', 'svg'].includes(ext)
+      ) {
+        const filePath = `${root}${pathname}`
+        let exist = true
+        try {
+          await Deno.stat(filePath)
+        } catch (_) {
+          exist = false
+        }
+        if (!exist) {
+          return new Response(null, {
+            status: 404,
+          })
+        }
+        const file = await Deno.readFile(filePath)
+        return new Response(compress(file), {
+          headers: {
+            'content-type': mimeTypes(ext),
+            'content-encoding': 'br',
+          },
+        })
+      }
+      return serveDir(req, {
+        fsRoot: root,
       })
-      : handler(req, ctx)
+    }
+    return handler(req, ctx)
   }, {
     signal,
     port,
