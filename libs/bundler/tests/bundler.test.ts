c92706bf44bb4cf1e3b3473b99e9e52905b87e89
Edward Irby <e.irby@pm.me>
2023-02-18 15:14:50 -0600

    ci: trying to fix async leak

--- libs/bundler/tests/bundler.test.ts
+++ libs/bundler/tests/bundler.test.ts
@@ -9,23 +9,23 @@ Deno.test('bundler()', async (t) => {
   await t.step('setup', async () => {
     await Deno.mkdir(registryDir, { recursive: true })
   })
-  // await t.step('bundle', async () => {
-  const build = bundler({
-    dev: false,
-    entryPoints: [`${root}/test.island.ts`],
-  })
-  const content = await build()
-  await Deno.mkdir(registryDir, { recursive: true })
-  if (content && Array.isArray(content)) {
-    for (const entry of content) {
-      const filePath = `${registryDir}${entry[0]}`
-      await Deno.writeFile(filePath, entry[1])
-      const fileInfo = await Deno.stat(filePath)
-      assert(fileInfo.isFile)
-      assert(fileInfo.size > 0)
+  await t.step('bundle', async () => {
+    const build = bundler({
+      dev: false,
+      entryPoints: [`${root}/test.island.ts`],
+    })
+    const content = await build()
+    await Deno.mkdir(registryDir, { recursive: true })
+    if (content && Array.isArray(content)) {
+      for (const entry of content) {
+        const filePath = `${registryDir}${entry[0]}`
+        await Deno.writeFile(filePath, entry[1])
+        const fileInfo = await Deno.stat(filePath)
+        assert(fileInfo.isFile)
+        assert(fileInfo.size > 0)
+      }
     }
-  }
-  // })
+  })
 
   await t.step('teardown', async () => {
     await Deno.remove(`${registryDir}/test.island.js`)
