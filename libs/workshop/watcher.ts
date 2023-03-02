53cf60e743afb48e0a74d4333660049be066d37e
Edward Irby <e.irby@pm.me>
2023-03-02 00:50:20 -0600

    fix(workshop): double reload

--- libs/workshop/watcher.ts
+++ libs/workshop/watcher.ts
@@ -1,5 +1,4 @@
 import { Watcher } from './types.ts'
-import { wait } from '../utils/mod.ts'
 export const watcher: Watcher = async ({
   writeFn,
   getRoutes,
@@ -8,22 +7,15 @@ export const watcher: Watcher = async ({
   ...rest
 }) => {
   const watcher = Deno.watchFs(workspace, { recursive: true })
-  let lastEvent = ''
   for await (const { kind } of watcher) {
-    if (['any', 'access'].includes(kind)) {
-      lastEvent = kind
-      continue
-    }
-    if (kind !== lastEvent) {
+    if (kind === 'modify') {
       const obj = await writeFn()
-      await wait(500)
       routes.clear()
       const newRoutes = await getRoutes({
         ...obj,
         ...rest,
       })
       newRoutes.forEach((val, key) => routes.set(key, val))
-      lastEvent = kind
     }
   }
 }
