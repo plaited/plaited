5f1d26d3577c51b30483026528bdbd518439e27a
Edward Irby <e.irby@pm.me>
2023-05-02 23:05:05 -0500

    ci: better than last try, now to try just installing  playwright deps

--- test.sh
+++ test.sh
@@ -6,5 +6,8 @@ bun install
 # Run yarn build
 npm run build --workspaces --if-present
 
+# Playwright download browsers
+npx playwright install  
+
 # Run test
 npm run test --workspaces --if-present
