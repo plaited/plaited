4594bbce3501ba9760a0aee16c9cfc761fa98e11
Edward Irby <e.irby@pm.me>
2023-06-03 15:28:31 -0500

    refactor: moving back utils

---
+++ libs/utils/src/key-mirror.ts
@@ -0,0 +1,13 @@
+export type KeyMirror<Keys extends string[]> = {
+  readonly [K in Keys[number]]: K;
+};
+
+/** create an object who's keys and values are the same by simply passing in the keys as arguments */
+export const keyMirror = <Keys extends string[]>(...inputs: Keys) => {
+  const mirrored = inputs.reduce(
+    (acc, key) => ({ ...acc, [key]: key }),
+    {} as KeyMirror<Keys>
+  )
+
+  return Object.freeze(mirrored)
+}
