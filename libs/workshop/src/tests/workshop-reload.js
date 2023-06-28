9669606f056ea05f8fd22de1c9cc0fc8656b17fc
Edward Irby <e.irby@pm.me>
2023-06-28 07:45:54 -0700

    ci: fix git add and update readme

--- libs/workshop/src/tests/workshop-reload.js
+++
@@ -1,9 +0,0 @@
-import { workshop } from '../../dist/workshop.js'
-const server = await workshop({
-  exts: '.stories.tsx',
-  srcDir: 'src/tests/__mocks__/get-story-valid',
-  testDir: '.playwright',
-  reload: true,
-})
-
-server.start()
