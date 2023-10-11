40906d3064005e82558a7df0603887c2fa298b9a
Edward Irby <EdwardIrby@users.noreply.github.com>
2023-10-10 22:25:02 -0700

    eirb/workshop-to-assembl (#72)
    * test(workshop): update snapshot
    
    * refactor: moving workshop out of monorepo into assembl repo

--- libs/workshop/src/tests/to-id.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from'@jest/globals'
-import { toId } from '../utils.js'
-
-test('toId', () => {
-  const actual = toId('Example/Element', 'basic')
-  expect(actual).toBe('example-element--basic')
-})
