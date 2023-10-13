b540d3720cab02475ad7e388813abdcaa30188fe
Edward Irby <e.irby@pm.me>
2023-10-13 08:07:13 -0700

    chore(plaited): calling plait method could be an asny function

--- libs/plaited/src/isle.ts
+++ libs/plaited/src/isle.ts
@@ -83,7 +83,7 @@ export const isle = (
                   ...bProgramOptions,
                 }
               )
-              this.plait({
+              void this.plait({
                 $: this.$.bind(this),
                 host: this,
                 trigger,
