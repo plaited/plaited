852003ef0839739f2b179bf78ab189e1ee5322b2
Edward Irby <e.irby@pm.me>
2023-11-04 11:37:13 -0700

    refactor: repuposing examples as test packages for various libs

--- examples/calculator/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Calculator } from './calculator.js'
-customElements.define(Calculator.tag, Calculator)
-const calc = document.createElement(Calculator.tag)
-const body = document.querySelector('body')
-body.appendChild(calc)
