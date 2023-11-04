3c663df7c7d221dd2b4c379e7845eaf58b7757d6
Edward Irby <e.irby@pm.me>
2023-11-03 22:22:09 -0700

    chore: add commitlint lerna scope

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,22 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [
-      2,
-      'always',
-      [
-        'behavioral',
-        'comms',
-        'component',
-        'examples',
-        'jsx',
-        'plaited',
-        'playbook',
-        'token-schema',
-        'token-transformer',
-        'token-types',
-        'utils',
-      ],
-    ],
-  },
+  extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
 }
