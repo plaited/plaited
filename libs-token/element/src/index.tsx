655aaab7e59ec1308eb6c1c77dcd4ea9ff1f9338
Edward Irby <e.irby@pm.me>
2023-12-09 22:00:00 -0800

    fix(miles): update to new css api

--- libs-token/element/src/index.tsx
+++ libs-token/element/src/index.tsx
@@ -2,14 +2,12 @@ import { Component } from '@plaited/component'
 import { css } from '@plaited/jsx'
 import { stylesheets } from '@plaited/jsx/utils'
 
-export const TokensTag: `${string}-${string}` = 'design-tokens'
-
 const { $stylesheet } = css`
   :host {
     display: contents;
   }
 `
-export const getTokenElement = (stylesheet: string, tag = TokensTag) => {
+export const getTokenElement = (stylesheet: string, tag: `${string}-${string}` = 'design-tokens') => {
   return class DesignTokensElement extends Component({
     tag,
     template: <slot stylesheet={stylesheets($stylesheet, stylesheet)}></slot>,
