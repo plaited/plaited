f4dc546ebdb4ce9d849f52c320c4f5a4fcdecd16
Edward Irby <e.irby@pm.me>
2024-04-21 23:12:15 -0700

    refactor: archiving miles again

--- libs/miles/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { applyStylesheet } from './utils.js'
-import { css as originalCss, Primitive } from 'plaited/css'
-
-export const css = (
-  strings: TemplateStringsArray,
-  ...expressions: Array<Primitive | Primitive[]>
-): Record<string, string> => applyStylesheet(originalCss(strings, ...expressions))
-
-export { classNames, stylesheets } from 'plaited/css'
