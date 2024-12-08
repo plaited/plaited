6df3d601154ae7ad0acc310da48618dafee1b0d1
Edward Irby <e.irby@pm.me>
2024-12-07 16:38:05 -0800

    refactor: rename regex

--- src/jsx/ssr.ts
+++ src/jsx/ssr.ts
@@ -23,9 +23,9 @@ export const ssr = (...templates: TemplateObject[]) => {
   const style = stylesheets.size ? `<style>${[...stylesheets].join('')}</style>` : ''
   const str = arr.join('')
   const headIndex = str.indexOf('</head>')
-  const startBodyRegex = /<body\b[^>]*>/i
-  const startBodyMatch = startBodyRegex.exec(str)
-  const startBodyIndex = startBodyMatch ? startBodyMatch.index + startBodyMatch[0].length : 0
-  const index = headIndex !== -1 ? headIndex : startBodyIndex
+  const bodyRegex = /<body\b[^>]*>/i
+  const bodyMatch = bodyRegex.exec(str)
+  const bodyIndex = bodyMatch ? bodyMatch.index + bodyMatch[0].length : 0
+  const index = headIndex !== -1 ? headIndex : bodyIndex
   return str.slice(0, index) + style + str.slice(index)
 }
