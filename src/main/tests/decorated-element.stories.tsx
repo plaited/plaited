0db81ee286b4aa16f1a8e089b2d3ed27cb3a1fcf
Edward Irby <e.irby@pm.me>
2025-03-23 03:55:08 -0700

    chore: migrate some new stubs overs

---
+++ src/main/tests/decorated-element.stories.tsx
@@ -0,0 +1,56 @@
+import { type StoryObj, type Args } from 'plaited/test'
+import { DecoratedCheckbox } from './decorated-element.js'
+
+export const example: StoryObj<Args<typeof DecoratedCheckbox>> = {
+  description: `
+    DecoratedCheckbox template uses a plaited template
+    to decorate an input element
+    of the type checkbox.
+  `,
+  template: DecoratedCheckbox,
+  async play({ findByAttribute, assert }) {
+    const input = document.querySelector('input')
+    let symbol = await findByAttribute('p-target', 'symbol')
+    let backgroundColor = getComputedStyle(symbol as Element).backgroundColor
+    assert({
+      given: 'render of decorated checkbox',
+      should: 'have light blue background',
+      expected: 'rgb(173, 216, 230)',
+      actual: backgroundColor,
+    })
+    input?.toggleAttribute('checked')
+    symbol = await findByAttribute('p-target', 'symbol')
+    backgroundColor = getComputedStyle(symbol as Element).backgroundColor
+    assert({
+      given: 'toggling slotted input checked attributed',
+      should: 'have blue background',
+      expected: 'rgb(0, 0, 255)',
+      actual: backgroundColor,
+    })
+  },
+}
+
+export const checked: StoryObj = {
+  description: `renders decorated checkbox checked`,
+  template: DecoratedCheckbox,
+  args: {
+    checked: true,
+  },
+}
+
+export const disabled: StoryObj = {
+  description: `renders decorated checkbox disabled`,
+  template: DecoratedCheckbox,
+  args: {
+    disabled: true,
+  },
+}
+
+export const disabledAndChecked: StoryObj = {
+  description: `renders decorated checkbox disabled and checked`,
+  template: DecoratedCheckbox,
+  args: {
+    disabled: true,
+    checked: true,
+  },
+}
---
+++ src/main/tests/popover-example.stories.tsx
@@ -0,0 +1,18 @@
+import { type StoryObj } from 'plaited/test'
+import { DecoratedPopover, DecoratedPopoverClose } from './popover-example.js'
+
+export const Example: StoryObj = {
+  description: `renders a decorated popover`,
+  template: () => (
+    <DecoratedPopover>
+      <button slot='popover-trigger'>Open Popover</button>
+
+      <div slot='popover-target'>
+        Greetings, one and all!
+        <DecoratedPopoverClose>
+          <button>X</button>
+        </DecoratedPopoverClose>
+      </div>
+    </DecoratedPopover>
+  ),
+}
