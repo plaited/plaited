21f2ecbf123a562cc8302ad0b6781440d40bfdf3
Edward Irby <e.irby@pm.me>
2025-06-21 14:57:26 -0700

    chore: cleanup

--- src/workshop/mcp/mcp.utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ServerResult } from '@modelcontextprotocol/sdk/types.js'
-import { ueid } from '../../utils.js'
-
-export const mcpPromisesMap = new Map<
-  string,
-  { reject: (value: unknown) => void; resolve: (result: ServerResult) => void }
->()
-
-export const createRequestId = () => ueid('mcp_')
