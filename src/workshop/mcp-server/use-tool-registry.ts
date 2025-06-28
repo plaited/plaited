3d5effdb232e28bf5b85acb52beea08a48d1a3e1
Edward Irby <e.irby@pm.me>
2025-06-27 22:57:27 -0700

    --wip-- [skip ci]

--- src/workshop/mcp-server/use-tool-registry.ts
+++ src/workshop/mcp-server/use-tool-registry.ts
@@ -3,7 +3,7 @@ import { type CallToolResult } from '@modelcontextprotocol/sdk/types.js'
 import { type SignalWithoutInitialValue } from '../../behavioral.js'
 import { ueid } from '../../utils.js'
 import { useSignal } from '../../behavioral.js'
-import type { ZodRawShape } from 'zod'
+import { z } from 'zod'
 
 export const mcpPromisesMap = new Map<
   string,
@@ -12,24 +12,42 @@ export const mcpPromisesMap = new Map<
 
 export const createRequestReference = () => ueid('mcp_')
 
-type ToolSignalCallback<T extends ZodRawShape = ZodRawShape> = ToolCallback<T>
+type InputSchemma = Parameters<McpServer['registerTool']>[1]['inputSchema']
 
-type ToolSignal = SignalWithoutInitialValue<{
+type ToolSignalCallback<T extends InputSchemma = InputSchemma> = T
+
+type ToolConfig = Parameters<McpServer['registerTool']>[1]
+
+type ToolSignal<T extends InputSchemma = undefined> = SignalWithoutInitialValue<{
   resolve: ReturnType<typeof Promise.withResolvers<CallToolResult>>['resolve']
   reject: ReturnType<typeof Promise.withResolvers<CallToolResult>>['reject']
-  args: Parameters<ToolSignalCallback>[0]
+  args: T
 }>
 
 type ToolRegistry = {
   [k: string]: Parameters<McpServer['registerTool']>[1]
 }
 
-const registerTool = (
-  server: McpServer,
-  type: Parameters<McpServer['registerTool']>[0],
-  config: Parameters<McpServer['registerTool']>[1],
-): [ToolSignal, RegisteredTool] => {
-  const signal: ToolSignal = useSignal()
+type InputArgs<S extends z.ZodRawShape> = S extends z.ZodRawShape ? z.infer<z.ZodObject<S>> : unknown
+
+type UseToolRegstritryReturn<T extends ToolRegistry> = {
+  [K in keyof T]: {
+    args: T[K] extends { inputSchema: infer S } ? InputArgs<S[]> : unknown
+    resolve: ReturnType<typeof Promise.withResolvers<CallToolResult>>['resolve']
+    reject: ReturnType<typeof Promise.withResolvers<CallToolResult>>['reject']
+  }
+}
+
+const registerTool = ({
+  server,
+  type,
+  config,
+}: {
+  server: McpServer
+  type: Parameters<McpServer['registerTool']>[0]
+  config: Parameters<McpServer['registerTool']>[1]
+}): [ToolSignal<typeof config.inputSchema>, RegisteredTool] => {
+  const signal: ToolSignal<typeof config.inputSchema> = useSignal()
   const tool = server.registerTool(type, config, async (args) => {
     const { promise, resolve, reject } = Promise.withResolvers<CallToolResult>()
     signal.set({
@@ -42,12 +60,16 @@ const registerTool = (
   return [signal, tool]
 }
 
-export const useToolRegistry = (server: McpServer, registry: ToolRegistry) => {
-  const target = {}
-  for (const [key, value] of Object.entries(registry)) {
-    Object.assign(target, {
-      [key]: registerTool(server, key, value),
-    })
+export const useToolRegistry = <T extends ToolRegistry>(server: McpServer, registry: T) => {
+  const target: UseToolRegstritryReturn<T> = {}
+  for (const [type, config] of Object.entries(registry)) {
+    target[key] = registerTool<
+      (typeof config)['inputSchema'] extends { inputSchema: infer S } ?
+        S extends z.ZodRawShape ?
+          z.infer<z.ZodObject<S>>
+        : unknown
+      : unknown
+    >({ type, config, server })
   }
   return target
 }
