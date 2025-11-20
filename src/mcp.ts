a0578480832ec1a33ed31b1ad6986360056ccea4
Edward Irby <e.irby@pm.me>
2025-11-19 21:15:29 -0800

    chore: cleanup

--- src/mcp.ts
+++
@@ -1,4 +0,0 @@
-// export * from './old-mcp/b-server.old.js'
-// export * from './old-mcp/b-server.utils.js'
-// export * from './old-mcp/b-server.utils.js'
-// export * from './old-mcp/mcp.types.js'
