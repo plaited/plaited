ba88bdbf09447a899bd8bc682aa2ade6292f7b17
Ali Stump <ali@alisonstump.dev>
2025-12-03 17:08:55 -0800

    fix(orchestrator-validation): fix test template

--- src/main/tests/orchestrator-validation.stories.tsx
+++ src/main/tests/orchestrator-validation.stories.tsx
@@ -1,20 +1,10 @@
 import type { FT } from 'plaited'
 import { story } from 'plaited/testing'
 
-const MyTestTemplate: FT = () => (
+const TestFixture: FT = () => (
   <div>
     <p>This is a test template inside the orchestrator fixture.</p>
   </div>
-)
-
-const TestFixture: FT = () => (
-  <plaited-orchestrator>
-    <plaited-header slot='header' />
-    <plaited-fixture slot='fixture'>
-      <MyTestTemplate />
-    </plaited-fixture>
-    <plaited-mask slot='mask' />
-  </plaited-orchestrator>
 )
 
 export const orchestratorValidation = story({
