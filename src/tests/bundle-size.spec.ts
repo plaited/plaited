b8ee0389963980e13f96b96f2ef21c5a08019de4
Edward Irby <e.irby@pm.me>
2025-12-24 14:47:51 -0800

    test: main kitchen sink export size test

--- src/tests/bundle-size.spec.ts
+++ src/tests/bundle-size.spec.ts
@@ -16,3 +16,19 @@ test('bElement export footprint < 5.1kb', async () => {
     console.log(`Plaited minimum size: ${size}kb`)
   }
 })
+
+test('main (kitchen sink) export footprint < 7.5kb', async () => {
+  const plaitedResults = await Bun.build({
+    entrypoints: [path.resolve(import.meta.dir, '../main.ts')],
+    minify: true,
+  })
+
+  for (const result of plaitedResults.outputs) {
+    // Can be consumed as blobs
+    const str = await result.text()
+    const compressed = Bun.gzipSync(Buffer.from(str))
+    const size = compressed.byteLength / 1024
+    expect(size).toBeLessThan(7.5)
+    console.log(`Plaited minimum size: ${size}kb`)
+  }
+})
