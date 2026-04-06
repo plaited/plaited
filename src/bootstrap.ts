c5448834fd44e04ca18b148728ec50bdd988c033
Edward Irby <e.irby@pm.me>
2026-04-05 21:32:23 -0700

    refactor: get rid of bootstrpa and rename

--- src/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-export * from './bootstrap/bootstrap.schemas.ts'
-export * from './bootstrap/bootstrap.ts'
-export type * from './bootstrap/bootstrap.types.ts'
-export * from './bootstrap/bootstrap-cli.ts'
