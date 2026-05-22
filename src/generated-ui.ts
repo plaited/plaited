b0a3a3009a10dcbbed4d6f2b30a105ad202768e0
Edward Irby <e.irby@pm.me>
2026-05-22 01:22:38 -0700

    chore: remove this ui approach

--- src/generated-ui.ts
+++
@@ -1,2 +0,0 @@
-export * from './generated-ui/generated-ui.schemas.ts'
-export * from './generated-ui/generated-ui.ts'
--- src/projection.ts
+++
@@ -1,3 +0,0 @@
-export * from './projection/projection.constants.ts'
-export * from './projection/projection.schemas.ts'
-export * from './projection/projection.ts'
