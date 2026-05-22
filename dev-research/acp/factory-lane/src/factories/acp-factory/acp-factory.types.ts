315039f78d389c49fac2552495db0ac12326dc1f
Edward Irby <e.irby@pm.me>
2026-05-22 01:23:53 -0700

    chore: removing acp scripts

--- dev-research/acp/factory-lane/src/factories/acp-factory/acp-factory.types.ts
+++
@@ -1,7 +0,0 @@
-export type CreateAcpFactoryOptions = {
-  stateSignalKey?: string
-  toolRegistrySignalKey?: string
-  workflowStateSignalKey?: string
-  transport?: 'stdio' | 'local_http'
-  maxSessions?: number
-}
